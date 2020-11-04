54c8848e6f6dafdfa2067240b3cb8d54b0a0ed91
kamal <kamal@glints.com>
2020-11-04 22:35:19 +0800

    update utils

--- src/utils.js
+++ src/utils.js
@@ -1,15 +1,31 @@
 export function getBrowserInfo() {
-    var userAgent = navigator.userAgent, temp,
-        matches = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    if (!navigator || !navigator.userAgent) {
+        return ""
+    }
+    var userAgent = navigator.userAgent;
+    var matches = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+    var temp;
+    console.log(matches)
     if (/trident/i.test(matches[1])) {
         temp = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
-        return 'IE ' + (temp[1] || '');
+        return {
+            name: 'IE',
+            version: parseInt(temp[1])
+        }
     }
     if (matches[1] === 'Chrome') {
         temp = userAgent.match(/\b(OPR|Edge)\/(\d+)/);
-        if (temp != null) return temp.slice(1).join(' ').replace('OPR', 'Opera');
+        if (temp != null) {
+            return {
+                name: 'Opera',
+                version: parseInt(temp[1])
+            }
+        }
     }
     matches = matches[2] ? [matches[1], matches[2]] : [navigator.appName, navigator.appVersion, '-?'];
     if ((temp = userAgent.match(/version\/(\d+)/i)) != null) matches.splice(1, 1, temp[1]);
-    return matches.join(' ');
+    return {
+        name: matches[0],
+        version: parseInt(matches[1])
+    }
 }
