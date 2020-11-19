411842f3f570ed693f935cf949e7fd700e6db2a3
kamal <kamal@glints.com>
2020-11-19 23:55:01 +0800

    Beautify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="hello">
     <h1>Your current device</h1>
-    <h2>userAgent: {{ navigator.userAgent }}</h2>
-    <h2>appVersion: {{ navigator.appVersion }}</h2>
-    <h2>appName: {{ navigator.appName }}</h2>
-    <h2>{{ info }}</h2>
+    <p>userAgent: {{ navigator.userAgent }}</p>
+    <p>appVersion: {{ navigator.appVersion }}</p>
+    <p>appName: {{ navigator.appName }}</p>
+    <h3>Browser: {{ info }}</h3>
     <v-divider></v-divider>
     <h1>Query browser info from input</h1>
     <v-text-field v-model="userAgent" label="userAgent"></v-text-field>
     <v-text-field v-model="appVersion" label="appVersion"></v-text-field>
     <v-text-field v-model="appName" label="appName"></v-text-field>
     <v-btn @click="handleQuery">Query</v-btn>
-    <h3>Ans {{ ans }}</h3>
+    <h3>Browser: {{ ans }}</h3>
   </div>
 </template>
 
@@ -48,7 +48,10 @@ export default {
 </script>
 
 <style scoped>
+h3 {
+  color: red;
+}
 div {
-  justify-content: center;
+  text-align: center;
 }
 </style>
