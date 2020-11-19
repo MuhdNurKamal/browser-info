ec553f9b5828ad9b259978b2ec6641fb87dfc9bb
kamal <kamal@glints.com>
2020-11-19 23:40:53 +0800

    Add query

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="hello">
-    <h1>userAgent: {{ navigator.userAgent }}</h1>
-    <h1>appVersion: {{ navigator.appVersion }}</h1>
-    <h1>appName: {{ navigator.appName }}</h1>
-    <h1>{{ info }}</h1>
+    <h1>Your current device</h1>
+    <h2>userAgent: {{ navigator.userAgent }}</h2>
+    <h2>appVersion: {{ navigator.appVersion }}</h2>
+    <h2>appName: {{ navigator.appName }}</h2>
+    <h2>{{ info }}</h2>
+    <v-divider></v-divider>
+    <h1>Query browser info from input</h1>
+    <v-text-field v-model="userAgent" label="userAgent"></v-text-field>
+    <v-text-field v-model="appVersion" label="appVersion"></v-text-field>
+    <v-text-field v-model="appName" label="appName"></v-text-field>
+    <v-btn @click="handleQuery">Query</v-btn>
+    <h3>Ans {{ ans }}</h3>
   </div>
 </template>
 
@@ -17,7 +25,30 @@ export default {
     msg: String,
   },
   data: () => {
-    return { info, navigator: navigator };
+    return {
+      info,
+      navigator: navigator,
+      userAgent: "",
+      appVersion: "",
+      appName: "",
+      ans: "",
+    };
+  },
+  methods: {
+    handleQuery: function() {
+      const nav = {
+        userAgent: this.userAgent,
+        appName: this.appName,
+        appVersion: this.appVersion,
+      };
+      this.ans = getBrowserInfo(nav);
+    },
   },
 };
 </script>
+
+<style scoped>
+div {
+  justify-content: center;
+}
+</style>
--- src/utils.ts
+++ src/utils.ts
@@ -1,40 +1,50 @@
-export function getBrowserInfo() {
-    if (!navigator || !navigator.userAgent) {
-        return ""
-    }
-    var userAgent = navigator.userAgent;
-    var matches = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-    var temp;
+export function getBrowserInfo(navInput: Navigator) {
+  let nav = navigator;
+  if (navInput) {
+    nav = navInput;
+  }
+  if (!nav || !nav.userAgent) {
+    return "";
+  }
+  var userAgent = nav.userAgent;
+  var matches =
+    userAgent.match(
+      /(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i
+    ) || [];
+  var temp;
 
-    if (/trident/i.test(matches[1])) {
-        temp = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
-        return {
-            name: 'IE',
-            version: parseInt(temp[1])
-        }
-    }
-    if (matches[1] === 'Chrome') {
-        temp = userAgent.match(/\b(OPR|Edge)\/(\d+)/);
-        if (temp != null) {
-            return {
-                name: temp[0],
-                version: parseInt(temp[1])
-            }
-        }
-    }
-    matches = matches[2] ? [matches[1], matches[2]] : [navigator.appName, navigator.appVersion, '-?'];
-    if ((temp = userAgent.match(/version\/(\d+)/i)) != null) matches.splice(1, 1, temp[1]);
+  if (/trident/i.test(matches[1])) {
+    temp = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
     return {
-        name: matches[0],
-        version: parseInt(matches[1])
+      name: "IE",
+      version: parseInt(temp[1]),
+    };
+  }
+  if (matches[1] === "Chrome") {
+    temp = userAgent.match(/\b(OPR|Edge)\/(\d+)/);
+    if (temp != null) {
+      return {
+        name: temp[0],
+        version: parseInt(temp[1]),
+      };
     }
+  }
+  matches = matches[2]
+    ? [matches[1], matches[2]]
+    : [nav.appName, nav.appVersion, "-?"];
+  if ((temp = userAgent.match(/version\/(\d+)/i)) != null)
+    matches.splice(1, 1, temp[1]);
+  return {
+    name: matches[0],
+    version: parseInt(matches[1]),
+  };
 }
 
 export function getNavigator() {
-    const x = {
-        userAgent: navigator.userAgent,
-        appVersion: navigator.appVersion,
-        appName: navigator.appName,
-    }
-    return x;
+  const x = {
+    userAgent: navigator.userAgent,
+    appVersion: navigator.appVersion,
+    appName: navigator.appName,
+  };
+  return x;
 }
